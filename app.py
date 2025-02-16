51f9e3090a0b2c590ef1359a4b684755c57ad5dc
hairxplorer <lbccoin13@gmail.com>
2025-02-16 18:18:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -238,7 +238,7 @@ async def analyze(
         client = AsyncOpenAI(api_key=os.getenv("OPENAI_API_KEY"))
         try:
             response = await client.chat.completions.create(
-                model="gpt-4-turbo",  # Utilisation d'un modèle textuel
+                model="gpt-4o-mini",  # Utilisation d'un modèle textuel
                 messages=[
                     {
                         "role": "user",
